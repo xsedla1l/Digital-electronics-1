0a72d91daffdd41019152b6ad4d5f9fa22459c05
xsedla1l <marek.sedlacek11@gmail.com>
2021-03-04 14:34:10 +0100

    Pokracovanie

--- Labs/hex_7seg/hex_7seg.srcs/sources_1/new/top.vhd
+++ Labs/hex_7seg/hex_7seg.srcs/sources_1/new/top.vhd
@@ -67,4 +67,19 @@ begin
          AN     <= b"1111_0111";
          
          LED(3 downto 0) <= SW;
+         
+             -- Turn LED(4) on if input value is equal to 0, ie "0000"
+    LED(4) <= '0' when(SW = "0000"); 
+    
+    -- Turn LED(5) on if input value is greater than "1001"
+    LED(5) <= '0' when(SW > "1001"); 
+    
+    -- Turn LED(6) on if input value is odd, ie 1, 3, 5, ...
+    --LED(6) <= 
+    
+    -- Turn LED(7) on if input value is a power of two, ie 1, 2, 4, or 8
+     LED(7) <=   '0' when (SW = "0001") else
+                 '0' when (SW = "0010") else
+                 '0' when (SW = "0100") else
+                 '0' when (SW = "1000");  
 end Behavioral;
