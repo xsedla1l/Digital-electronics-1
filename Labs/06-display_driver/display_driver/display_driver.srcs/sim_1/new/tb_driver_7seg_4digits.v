2a03e27c1369c966bee606c54c85eecb23d45508
xsedla1l <marek.sedlacek11@gmail.com>
2021-03-19 22:16:45 +0100

    rozpracovane

---
+++ Labs/06-display_driver/display_driver/display_driver.srcs/sim_1/new/tb_driver_7seg_4digits.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.03.2021 10:47:51
+// Design Name: 
+// Module Name: tb_driver_7seg_4digits
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_driver_7seg_4digits(
+
+    );
+endmodule
