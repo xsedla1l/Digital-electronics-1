a7d656cc94bf8d2a91bd50f10fbd3f2346f53571
xsedla1l <marek.sedlacek11@gmail.com>
2021-04-05 13:38:19 +0200

    rozpracovane

--- Labs/08-traffic_lights/traffic/traffic.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/xsim_webtalk.tcl
+++ Labs/08-traffic_lights/traffic/traffic.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/marek/Desktop/skola/4semester/DE1/Digital-electronics-1/Labs/08-traffic_lights/traffic/traffic.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Apr  5 11:22:14 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Apr  5 11:48:29 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "fed8f752-9922-4c7e-99c7-5286cdb71516" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "152745cf150d46cab1a4d918cb3825c6" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "8" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "9" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 7 4700U with Radeon Graphics         " -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "10 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.09_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6596_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3550925952 -regid "" -xml C:/Users/marek/Desktop/skola/4semester/DE1/Digital-electronics-1/Labs/08-traffic_lights/traffic/traffic.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/marek/Desktop/skola/4semester/DE1/Digital-electronics-1/Labs/08-traffic_lights/traffic/traffic.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/marek/Desktop/skola/4semester/DE1/Digital-electronics-1/Labs/08-traffic_lights/traffic/traffic.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.06_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6580_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1673215683 -regid "" -xml C:/Users/marek/Desktop/skola/4semester/DE1/Digital-electronics-1/Labs/08-traffic_lights/traffic/traffic.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/marek/Desktop/skola/4semester/DE1/Digital-electronics-1/Labs/08-traffic_lights/traffic/traffic.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/marek/Desktop/skola/4semester/DE1/Digital-electronics-1/Labs/08-traffic_lights/traffic/traffic.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- Labs/08-traffic_lights/traffic/traffic.srcs/sources_1/new/p_smart_traffic_fsm.vhd
+++ Labs/08-traffic_lights/traffic/traffic.srcs/sources_1/new/p_smart_traffic_fsm.vhd
@@ -1,33 +1,109 @@
-# Trafic
-# 1) 
-## A) State table
-| **Input P** | `0` | `0` | `1` | `1` | `0` | `1` | `0` | `1` | `1` | `1` | `1` | `0` | `0` | `1` | `1` | `1` |
-| :-- | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: |
-| **Clock** | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) |
-| **State** | A | A | B | C | C | D | A | B | C | D | D | A | A | B | C | D |
-| **Output R** | `0` | `0` | `0` | `0` | `0` | `1` | `0` | `0` | `0` | `1` | `1` | `0` | `0` | `0` | `0` | `1` |
+------------------------------------------------------------------------
+--
+-- Traffic light controller using FSM.
+-- Nexys A7-50T, Vivado v2020.1.1, EDA Playground
+--
+-- Copyright (c) 2020-Present Tomas Fryza
+-- Dept. of Radio Electronics, Brno University of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+-- This code is inspired by:
+-- [1] LBEbooks, Lesson 92 - Example 62: Traffic Light Controller
+--     https://www.youtube.com/watch?v=6_Rotnw1hFM
+-- [2] David Williams, Implementing a Finite State Machine in VHDL
+--     https://www.allaboutcircuits.com/technical-articles/implementing-a-finite-state-machine-in-vhdl/
+-- [3] VHDLwhiz, One-process vs two-process vs three-process state machine
+--     https://vhdlwhiz.com/n-process-state-machine/
+--
+------------------------------------------------------------------------
 
-## B) Nexys A7 board
-![alt text](https://github.com/xsedla1l/Digital-electronics-1/blob/main/Labs/08-traffic_lights/Images/image1.png)
- ## Table with color settings
- | **RGB LED** | **Artix-7 pin names** | **Red** | **Yellow** | **Green** |
-| :-: | :-: | :-: | :-: | :-: |
-| LD16 | N15, M16, R12 | `1,0,0` | `1,1,0` | `0,1,0` |
-| LD17 | N16, R11, G14 | `1,0,0` | `1,1,0` | `0,1,0` |
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-# 2) 
-## A) State diagram
-![alt text](https://github.com/xsedla1l/Digital-electronics-1/blob/main/Labs/08-traffic_lights/Images/image2.jpg)
+------------------------------------------------------------------------
+-- Entity declaration for traffic light controller
+------------------------------------------------------------------------
+entity p_smart_traffic_fsm is
+    port(
+        clk     : in  std_logic;
+        reset   : in  std_logic;
+        sensS   : in  std_logic;    -- sensor for South
+        sensW   : in  std_logic;    -- sensor fot West
+        -- Traffic lights (RGB LEDs) for two directions
+        south_o : out std_logic_vector(3 - 1 downto 0);
+        west_o  : out std_logic_vector(3 - 1 downto 0)
+    );
+end entity p_smart_traffic_fsm;
 
-## B) Listing of VHDL code of sequential process p_traffic_fsm
-```VHDL
-p_traffic_fsm : process(clk)
+------------------------------------------------------------------------
+-- Architecture declaration for traffic light controller
+------------------------------------------------------------------------
+architecture Behavioral of p_smart_traffic_fsm is
+
+    -- Define the states
+    type t_state is (STOP1,
+                     WEST_GO,
+                     WEST_WAIT,
+                     STOP2,
+                     SOUTH_GO,
+                     SOUTH_WAIT);
+    -- Define the signal that uses different states
+    signal s_state  : t_state;
+
+    -- Internal clock enable
+    signal s_en     : std_logic;
+    --  sensors signas
+    signal s_sensS     : std_logic;
+    signal s_sensW     : std_logic;
+    -- Local delay counter
+    signal   s_cnt  : unsigned(5 - 1 downto 0);
+
+    -- Specific values for local counter
+    constant c_DELAY_4SEC : unsigned(5 - 1 downto 0) := b"1_0000";
+    constant c_DELAY_2SEC : unsigned(5 - 1 downto 0) := b"0_1000";
+    constant c_DELAY_1SEC : unsigned(5 - 1 downto 0) := b"0_0100";
+    constant c_ZERO       : unsigned(5 - 1 downto 0) := b"0_0000";
+
+    -- Output values
+    constant c_RED        : std_logic_vector(3 - 1 downto 0) := b"100";
+    constant c_YELLOW     : std_logic_vector(3 - 1 downto 0) := b"110";
+    constant c_GREEN      : std_logic_vector(3 - 1 downto 0) := b"010";
+
+begin
+
+    --------------------------------------------------------------------
+    -- Instance (copy) of clock_enable entity generates an enable pulse
+    -- every 250 ms (4 Hz). Remember that the frequency of the clock 
+    -- signal is 100 MHz.
+    
+    -- JUST FOR SHORTER/FASTER SIMULATION
+    s_en <= '1';
+--    clk_en0 : entity work.clock_enable
+--        generic map(
+--            g_MAX =>        -- g_MAX = 250 ms / (1/100 MHz)
+--        )
+--        port map(
+--            clk   => clk,
+--            reset => reset,
+--            ce_o  => s_en
+--        );
+
+    --------------------------------------------------------------------
+    -- p_traffic_fsm:
+    -- The sequential process with synchronous reset and clock_enable 
+    -- entirely controls the s_state signal by CASE statement.
+    --------------------------------------------------------------------
+    p_p_smart_traffic_fsm : process(clk)
     begin
         if rising_edge(clk) then
             if (reset = '1') then       -- Synchronous reset
                 s_state <= STOP1 ;      -- Set initial state
                 s_cnt   <= c_ZERO;      -- Clear all bits
-
+            
+                      
+            
+            
             elsif (s_en = '1') then
                 -- Every 250 ms, CASE checks the value of the s_state 
                 -- variable and changes to the next state according 
@@ -110,41 +186,46 @@ p_traffic_fsm : process(clk)
                 end case;
             end if; -- Synchronous reset
         end if; -- Rising edge
-    end process p_traffic_fsm;
-```
+    end process p_p_smart_traffic_fsm;
 
-## B) Listing of VHDL code of combinatorial process p_output_fsm
-```VHDL
-p_output_fsm : process(s_state)
+    --------------------------------------------------------------------
+    -- p_output_fsm:
+    -- The combinatorial process is sensitive to state changes, and sets
+    -- the output signals accordingly. This is an example of a Moore 
+    -- state machine because the output is set based on the active state.
+    --------------------------------------------------------------------
+    p_output_p_smart_traffic_fsm : process(s_state)
     begin
         case s_state is
             when STOP1 =>           -- we defined the states at the beginning of the code
                 south_o <= c_RED;   -- RED -> "100"
                 west_o  <= c_RED;
+                
             when WEST_GO =>
                 south_o <= c_RED;
                 west_o  <= c_GREEN; -- GREEN -> "010"
+                
             when WEST_WAIT =>
                 south_o <= c_RED;
                 west_o  <= c_YELLOW; -- YELLOW -> "110"
+                
             when STOP2 =>
                 south_o <= c_RED;
                 west_o  <= c_RED;
+                
             when SOUTH_GO =>
                 south_o <= c_GREEN;
                 west_o  <= c_RED;
+                
             when SOUTH_WAIT =>
                 south_o <= c_YELLOW;
-                west_o  <= c_RED;              
+                west_o  <= c_RED;   
+                           
             when others =>
                 south_o <= c_RED;
                 west_o  <= c_RED;
+                
         end case;
-    end process p_output_fsm;
-```
+    end process p_output_p_smart_traffic_fsm;
 
-## C) Screenshot 
-![alt text](https://github.com/xsedla1l/Digital-electronics-1/blob/main/Labs/08-traffic_lights/Images/image3.png)
-
-# 3)
-## A) 
+end architecture Behavioral;
